d55d8e84927d0e4150d5710a20d8405e66898205
effieMoran <eliana_474@hotmail.com>
2020-03-21 20:04:47 -0300

    Move to private variable

--- src/main/java/com/crowdar/examples/pages/phptravel/PHPTravelAboutUs.java
+++ src/main/java/com/crowdar/examples/pages/phptravel/PHPTravelAboutUs.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PHPTravelAboutUs extends PageBasePHPTravel {
 
+    private final String ABOUT_US_HEADER = "//*[@id=\"toggleStyle04-headingOne\"]/h5/a";
     public PHPTravelAboutUs(RemoteWebDriver driver) {
         super(driver);
         this.url = "/About-Us";
     }
 
     public WebElement getAboutUsHeader(String title) {
-        return driver.findElement(By.partialLinkText(title));
+        return driver.findElement(By.xpath(ABOUT_US_HEADER));
     }
-
 }
